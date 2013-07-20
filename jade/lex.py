06a3e23ee4e5a62e8efd5671c0f83c56e35b4f2f
Cheer Xiao <xiaqqaix@gmail.com>
2013-07-20 15:27:23 +0800

    lex.py: Fix error message of invalid tag

--- jade/lex.py
+++ jade/lex.py
@@ -224,8 +224,10 @@ class Lexer(AbstractLexer):
             return self.conclude(TAG), self.verbatim
         elif self.accept('|') or self.accept_run(self.valid_in_tags):
             return self.conclude(TAG), self.maybe_qualifier
-        else:
+        elif self.peek() in u'.#(':
             return self.conclude(TAG, omitempty=False), self.qualifier
+        else:
+            raise self.error('No valid tag found')
 
     def verbatim(self):
         """
