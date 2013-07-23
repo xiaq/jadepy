b369ba0e321534b865e56cf88e7511787155613c
Cheer Xiao <xiaqqaix@gmail.com>
2013-07-23 21:50:11 +0800

    flake8 fixes

--- jade/compile.py
+++ jade/compile.py
@@ -20,7 +20,7 @@ class Compiler(object):
         if isinstance(tag, HTMLTag):
             if tag.class_ or tag.id_ or tag.attr:
                 self.stream.write('<%s {{ _jade_attr(%r, %r, %r) }}>' %
-                             (tag.name, tag.id_, tag.class_, tag.attr))
+                                  (tag.name, tag.id_, tag.class_, tag.attr))
             else:
                 self.stream.write('<%s>' % tag.name)
         else:
@@ -43,12 +43,22 @@ doctypes = {
     '5': '<!DOCTYPE html>',
     'default': '<!DOCTYPE html>',
     'xml': '<?xml version="1.0" encoding="utf-8" ?>',
-    'transitional': '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">',
-    'strict': '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">',
-    'frameset': '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Frameset//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-frameset.dtd">',
-    '1.1': '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">',
-    'basic': '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML Basic 1.1//EN" "http://www.w3.org/TR/xhtml-basic/xhtml-basic11.dtd">',
-    'mobile': '<!DOCTYPE html PUBLIC "-//WAPFORUM//DTD XHTML Mobile 1.2//EN" "http://www.openmobilealliance.org/tech/DTD/xhtml-mobile12.dtd">'
+    'transitional': '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 '
+                    'Transitional//EN" "http://www.w3.org/TR/xhtml1/'
+                    'DTD/xhtml1-transitional.dtd">',
+    'strict': '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 '
+              'Strict//EN" "http://www.w3.org/TR/xhtml1/'
+              'DTD/xhtml1-strict.dtd">',
+    'frameset': '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 '
+                'Frameset//EN" "http://www.w3.org/TR/xhtml1/'
+                'DTD/xhtml1-frameset.dtd">',
+    '1.1': '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" '
+                '"http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">',
+    'basic': '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML Basic '
+             '1.1//EN" "http://www.w3.org/TR/xhtml-basic/xhtml-basic11.dtd">',
+    'mobile': '<!DOCTYPE html PUBLIC "-//WAPFORUM//DTD XHTML Mobile 1.2//EN" '
+              '"http://www.openmobilealliance.org/tech/DTD/'
+              'xhtml-mobile12.dtd">'
 }
 
 
