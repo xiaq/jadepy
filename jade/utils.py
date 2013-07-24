a363663cfede4aa9b6677e1b9d740b7198ea3374
Cheer Xiao <xiaqqaix@gmail.com>
2013-07-24 14:20:57 +0800

    utils.py: Document repr_calling

--- jade/utils.py
+++ jade/utils.py
@@ -3,6 +3,9 @@ def has_proper_prefix(s, prefix):
 
 
 def repr_calling(args, kwargs):
+    """
+    Reconstruct function calling code.
+    """
     li = []
     li.extend(repr(a) for a in args)
     li.extend('%s=%r' % (k, v) for k, v in kwargs.items())
